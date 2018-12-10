cdeb3911dffd2889440f6690ddbb08f3d53230ec
Tobias Andersen <j.tobiasandersen@gmail.com>
2018-12-10 01:56:06 +0100

    Updates

---
+++ component---src-pages-404-js-97c85c99fa35cc8ebbf5.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{132:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),s=n(143);t.default=function(){return u.a.createElement(s.a,null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-97c85c99fa35cc8ebbf5.js.map
